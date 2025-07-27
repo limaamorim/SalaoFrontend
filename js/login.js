f8cc0a78e550fbb18e392314c60f94ac0d5dd301
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-07-27 19:11:00 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const password = document.getElementById('password').value;
 
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://salaobackend-1.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
